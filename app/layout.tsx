abc3178825489615c75861dfa934ca325a345320
Serhii Balaba <balabaserhii@gmail.com>
2025-12-04 21:20:35 +0100

    back

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,7 @@ import ButtonWA from "@/components/ButtonWA";
 import { Toaster } from "@/components/ui/sonner";
 import { StructuredData } from "./lib/structured-data";
 import CookiebotLoader from "@/components/CookiebotLoader";
-<<<<<<< HEAD
-import { GoogleAnalyticsWithCookiebot } from "@/components/GoogleAnalytics";
 
-=======
->>>>>>> parent of e52794e (гуггл аналитика)
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -60,10 +56,6 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
-<<<<<<< HEAD
-        <GoogleAnalyticsWithCookiebot />
-=======
->>>>>>> parent of e52794e (гуггл аналитика)
         <link rel="canonical" href="https://www.balabastudio.de/" />
         <StructuredData />
       </head>
@@ -87,4 +79,3 @@ export default function RootLayout({
     </html>
   );
 }
-
